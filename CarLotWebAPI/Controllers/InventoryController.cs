d6d4cd1c06652c357e475170be7b2a4884493ed2
Vitalii <vpempus@gmail.com>
2020-08-28 13:46:18 +0300

    Api/GET

--- CarLotWebAPI/Controllers/InventoryController.cs
+++ CarLotWebAPI/Controllers/InventoryController.cs
@@ -13,16 +13,59 @@ namespace CarLotWebAPI.Controllers
     [RoutePrefix("api/Inventory")]
     public class InventoryController : ApiController
     {
-        [HttpGet, Route("")]
-        public IEnumerable<string> Get()
+        static IMapper mapper;
+        static InventoryController()
         {
-            return new string[] { "value1", "Value2" };
+            var config = new MapperConfiguration(cfg => {
+                cfg.CreateMap<Inventory, Inventory>()
+                .ForMember(x => x.Orders, opt => opt.Ignore());
+            });
+            mapper = config.CreateMapper();
+
         }
 
-        //GET api/values/5
-        public string Get (int id)
+        private readonly InventoryRepo _repo = new InventoryRepo();
+
+        //[HttpGet, Route("")]
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "Value2" };
+        //}
+
+        ////GET api/values/5
+        //public string Get (int id)
+        //{
+        //    return id.ToString();
+        //}
+
+        //GET: api/Inventory
+        [HttpGet, Route("")]
+        public IEnumerable<Inventory> GetInventory()
         {
-            return id.ToString();
+            var inventories = _repo.GetAll();
+            return mapper.Map<List<Inventory>,List <Inventory>>(inventories);
+        }
+
+        //GET: api/Inventory/5
+        [HttpGet, Route("{id}", Name = "DisplayRoute")]
+        [ResponseType(typeof(Inventory))]
+        public async Task<IHttpActionResult> GetInventory(int id)
+        {
+            Inventory inventory = _repo.GetOne(id);
+            if (inventory == null)
+            {
+                return NotFound();
+            }
+            return Ok(mapper.Map<Inventory, Inventory>(inventory));
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _repo.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
